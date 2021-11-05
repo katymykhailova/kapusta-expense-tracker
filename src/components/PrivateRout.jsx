06da3cacbf2b960697ec0a7e9d84a40fe42b9c5b
OlhaNikoliuk <nikoliukolha@gmail.com>
2021-11-05 20:19:17 +0200

    fixed react-router-dom

--- src/components/PrivateRout.jsx
+++ src/components/PrivateRout.jsx
@@ -1,10 +1,10 @@
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function PrivateRoute({ children, ...routeProps }) {
   // const isLoggedIn  = если пользователь  залогинен
   return (
     <Route {...routeProps}>
-      {isLoggedIn ? children : <Redirect to="/login" />}
+      {/* {isLoggedIn ? children : <Redirect to="/login" />} */}
     </Route>
   );
 }
