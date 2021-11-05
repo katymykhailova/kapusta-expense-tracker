582adec164dfebf2d397bebf3407e057d14b7edd
OlhaNikoliuk <nikoliukolha@gmail.com>
2021-11-05 13:31:28 +0200

    add route part

---
+++ src/components/PublicRoute.jsx
@@ -0,0 +1,18 @@
+import { Route, Redirect } from 'react-router-dom';
+
+function PublicRoute({
+  children,
+  restricted = false,
+  redirectTo = '/',
+  ...routeProps
+}) {
+  // const isLoggedIn  = если пользователь  залогинен
+  const shouldRedirect = isLoggedIn && restricted;
+  return (
+    <Route {...routeProps}>
+      {shouldRedirect ? <Redirect to={redirectTo} /> : children}
+    </Route>
+  );
+}
+
+export default PublicRoute;
