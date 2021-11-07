234ebc5d168132e9db2b25568b494fdd4e119e1d
Maria Shvyrkova <shvyrkovamaria@gmail.com>
2021-11-07 10:03:27 +0200

    add bg-styles for public pages

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -1,5 +1,13 @@
+import { useLocation } from 'react-router';
 import s from '../MainContainer/MainContainer.module.css';
 
 export default function MainContainer({ children }) {
-  return <div className={s.bgContainer}>{children}</div>;
+  const location = useLocation();
+  const isPublicPage =
+    location.pathname === '/signup' || location.pathname === '/login';
+  return (
+    <div className={isPublicPage ? `${s.bgPublic}` : `${s.bgPrivate}`}>
+      {children}
+    </div>
+  );
 }
