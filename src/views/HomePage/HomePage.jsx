e27333afe172f60b0086d3f43e27222bc96bfcdc
Maria Shvyrkova <shvyrkovamaria@gmail.com>
2021-11-09 17:22:33 +0200

    change cabbage styles

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { getIsLoading } from '../../redux/auth';
-
 export default function HomePage() {
-  return <>{getIsLoading && <div>HOME PAGE</div>}</>;
+  return (
+    <>
+      <div style={{ height: '100vh' }}>HOME PAGE</div>
+    </>
+  );
 }
