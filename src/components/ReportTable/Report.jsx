6592f967f31d670a728b6f08d9496fcc7ab5c530
OlhaNikoliuk <nikoliukolha@gmail.com>
2021-11-09 17:16:00 +0200

    add styles

--- src/components/ReportTable/Report.jsx
+++ src/components/ReportTable/Report.jsx
@@ -2,11 +2,11 @@ import s from './Report.module.css';
 // import st from './ReportTable.module.css';
 import deleteBtn from '../../images/delete.svg';
 
-function Report() {
+function Report({ date, category, amount, descr }) {
   return (
-    <tbody className={s.tableBody} cellpadding="0" cellspacing="0">
-      {/* <div className={s.scrollDiv}> */}
-      {/* <tr className={s.tableTr}>
+    // <tbody className={s.tableBody} cellPadding="0" cellSpacing="0">
+    /* <div className={s.scrollDiv}> */
+    /* <tr className={s.tableTr}>
           <td className={`${s.tableTd}, ${s.tableDate}`}>07.11.2021</td>
           <td className={`${s.tableTd}, ${s.tableDesc}`}>Молоко</td>
           <td className={`${s.tableTd}, ${s.tableCat}`}>Продукты</td>
@@ -22,15 +22,35 @@ function Report() {
               />
             </button>
           </td>
-        </tr> */}
-      <tr className={s.tableTr}>
-        <div className={s.divWrap}>
-          <td className={`${s.tableTd}, ${s.tableDate}`}>07.11.2021</td>
-          <td className={`${s.tableTd}, ${s.tableDesc}`}>Молоко</td>
-        </div>
+        </tr> */
+    <tr className={s.tableTr}>
+      <div className={s.divWrap}>
+        <td className={`${s.tableTd}, ${s.tableDate}`}>{date}</td>
+        <td className={`${s.tableTd}, ${s.tableDesc}`}>{descr}</td>
+      </div>
 
-        <td className={`${s.tableTd}, ${s.tableCat}`}>Продукты</td>
-        <td className={`${s.tableTd}, ${s.tableSum}`}>25.00 грн.</td>
+      <td className={`${s.tableTd}, ${s.tableCat}`}>{category}</td>
+      <td className={`${s.tableTd}, ${s.tableSum}`}>{amount}</td>
+      <td className={`${s.tableTd}, ${s.tableDelet}`}>
+        <button type="button" className={s.deleteBtn}>
+          <img
+            src={deleteBtn}
+            alt="logout button"
+            width="18px"
+            height="18px"
+            className={s.deleteBtnImg}
+          />
+        </button>
+      </td>
+    </tr>
+
+    /* <tr className={`${s.tableTr}, ${s.tdWrap}`}>
+        <td className={`${s.tableTd}, ${s.tableDate}`}>{date}</td>
+        <td className={`${s.tableTd}, ${s.tableDesc}`}>{descr}</td>
+      </tr>
+      <tr className={`${s.tableTr}, ${s.mainTr}`}>
+        <td className={`${s.tableTd}, ${s.tableCat}`}>{category}</td>
+        <td className={`${s.tableTd}, ${s.tableSum}`}>{amount}</td>
         <td className={`${s.tableTd}, ${s.tableDelet}`}>
           <button type="button" className={s.deleteBtn}>
             <img
@@ -42,29 +62,8 @@ function Report() {
             />
           </button>
         </td>
-      </tr>
-      <tr className={s.tableTr}>
-        <div className={s.divWrap}>
-          <td className={`${s.tableTd}, ${s.tableDate}`}>07.11.2021</td>
-          <td className={`${s.tableTd}, ${s.tableDesc}`}>Молоко</td>
-        </div>
-
-        <td className={`${s.tableTd}, ${s.tableCat}`}>Продукты</td>
-        <td className={`${s.tableTd}, ${s.tableSum}`}>25.00 грн.</td>
-        <td className={`${s.tableTd}, ${s.tableDelet}`}>
-          <button type="button" className={s.deleteBtn}>
-            <img
-              src={deleteBtn}
-              alt="logout button"
-              width="18px"
-              height="18px"
-              className={s.deleteBtnImg}
-            />
-          </button>
-        </td>
-      </tr>
-      {/* </div> */}
-    </tbody>
+      </tr> */
+    // </tbody>
   );
 }
 
--- src/components/ReportTable/ReportTable.jsx
+++ src/components/ReportTable/ReportTable.jsx
@@ -2,9 +2,84 @@ import s from './ReportTable.module.css';
 
 import Report from './Report';
 
+const transactions = [
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+  {
+    date: '01.01.2021',
+    descr: 'Молоко',
+    category: 'Продукты',
+    amount: '45 грн.',
+  },
+];
+
 function ReportTable() {
   return (
-    <table className={s.table} cellpadding="0" cellspacing="0">
+    <table className={s.table} cellPadding="0" cellSpacing="0">
       <thead className={s.tableHead}>
         <tr className={s.tableRow}>
           <th className={`${s.tableHeadName}, ${s.tableDate}`}>Дата</th>
@@ -14,7 +89,16 @@ function ReportTable() {
           <th className={`${s.tableHeadName}, ${s.tableDelet}`}></th>
         </tr>
       </thead>
-      <Report></Report>
+      <tbody className={s.tableBody}>
+        {transactions.map(i => (
+          <Report
+            date={i.date}
+            category={i.category}
+            amount={i.amount}
+            descr={i.descr}
+          />
+        ))}
+      </tbody>
     </table>
   );
 }
