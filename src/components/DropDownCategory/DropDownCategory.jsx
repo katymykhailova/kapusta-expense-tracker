4faf616352d072d2fe65f69a9eb982e31ec4ee34
Maksim Tsopa <maks98242@gmail.com>
2021-11-11 21:41:22 +0200

    add form submit

--- src/components/DropDownCategory/DropDownCategory.jsx
+++ src/components/DropDownCategory/DropDownCategory.jsx
@@ -5,7 +5,15 @@ export default function DropDownCategory({ changerDescription }) {
   return (
     <div className={s.categoryPosition}>
       <ul>
-        <li className={s.categoryName} onClick={changerDescription}>
+        <li
+          className={s.categoryName}
+          // label="Транспорт"
+          value="Tranport"
+          onClick={changerDescription}
+          // onClick={() => {
+          //   changerDescription(value);
+          // }}
+        >
           Транспорт
         </li>
         <li className={s.categoryName} onClick={changerDescription}>
@@ -21,22 +29,3 @@ export default function DropDownCategory({ changerDescription }) {
     </div>
   );
 }
-
-// import s from './DropDownCategory.module.css';
-// import { createPortal } from 'react-dom';
-
-// const modalRoot = document.querySelector('#modal-root__category');
-
-// export default function DropDownCategory() {
-//   return createPortal(
-//     <div className={s.categoryPosition}>
-//       <ul>
-//         <li className={s.categoryName}>Транспорт</li>
-//         <li className={s.categoryName}>Продукты</li>
-//         <li className={s.categoryName}>Здоровье</li>
-//         <li className={s.categoryName}>Алкоголь</li>
-//       </ul>
-//     </div>,
-//     modalRoot,
-//   );
-// }
--- src/components/FormDescription/FormDescription.jsx
+++ src/components/FormDescription/FormDescription.jsx
@@ -3,26 +3,55 @@ import { useForm } from 'react-hook-form';
 import s from './FormDescription.module.css';
 import calculator from '../../images/calculator.svg';
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import DropDownCategory from '../DropDownCategory/DropDownCategory';
 
+// import ButtonBlock from '../ButtonBlock/ButtonBlock';
+
 export default function FormDescription() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = data => {
+    // console.log(data);
+    reset({
+      name: '',
+      categories: 'Категория товара',
+      sum: '',
+    });
+    console.log(data);
+  };
 
   const [open, setOpen] = useState(false);
   const [placeholder, setPlaceholder] = useState('Категория товара');
 
+  const ref = useRef();
+
+  useEffect(() => {
+    const checkClickOutside = e => {
+      if (open && ref.current && !ref.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener('click', checkClickOutside);
+    return () => {
+      document.removeEventListener('click', checkClickOutside);
+    };
+  }, [open]);
+
   const changerPlaceholder = e => {
+    console.log(e);
     setPlaceholder(e.target.innerText);
     setOpen(false);
   };
 
-  // const onSubmit2 = data => console.log('+');
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={s.formStyles}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      autoComplete="off"
+      className={s.formStyles}
+      ref={ref}
+    >
       <div className={s.productInfo}>
         <div className={s.datePosition}>
           <input {...register('date')} type="date" className={s.dateProduct} />
@@ -32,12 +61,14 @@ export default function FormDescription() {
             {...register('name')}
             className={s.inputProductName}
             placeholder="Описание товара"
+            // name="firstName"
           />
           <input
             {...register('categories')}
             className={s.inputСategoryName}
             // onClick={handleSubmit(onSubmit2)}
-            placeholder={placeholder}
+            // placeholder={placeholder}
+            value={placeholder}
             // value="Категория товара"
             onClick={() => setOpen(!open)}
             readOnly
@@ -53,8 +84,31 @@ export default function FormDescription() {
         </div>
       </div>
       <div className={s.btnPosition}>
-        <input type="submit" />
-        <input type="reset" />
+        <input
+          value="ввод"
+          type="submit"
+          // type="button"
+          // onClick={() => {
+          //   reset({
+          //     name: '',
+          //     categories: '',
+          //     sum: '',
+          //   });
+          // }}
+        />
+        <input
+          value="очистить"
+          // onClick={() => setPlaceholder('Категория товара')}
+          type="reset"
+          type="button"
+          // onClick={() => {
+          //   reset({
+          //     name: '',
+          //     categories: '',
+          //     sum: '',
+          //   });
+          // }}
+        />
       </div>
       {open && <DropDownCategory changerDescription={changerPlaceholder} />}
       {/* <DropDownCategory /> */}
