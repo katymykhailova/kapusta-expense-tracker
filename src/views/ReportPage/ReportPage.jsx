449d906e092f47047f204cb53c851241fb6e8d10
Nataly <natasha.totska@yahoo.com>
2021-11-13 21:42:43 +0200

    add

--- src/views/ReportPage/ReportPage.jsx
+++ src/views/ReportPage/ReportPage.jsx
@@ -11,21 +11,6 @@ import HeaderReport from '../../components/Reports/HeaderReport';
 import CurrentMonthReport from '../../components/Reports/CurrentMonthReport';
 import HeaderSection from '../../components/HeaderSection/HeaderSection';
 
-const arrMonths = [
-  { id: '1', name: 'Январь' },
-  { id: '2', name: 'Февраль' },
-  { id: '3', name: 'Март ' },
-  { id: '4', name: 'Апрель' },
-  { id: '5', name: 'Май' },
-  { id: '6', name: 'Июнь' },
-  { id: '7', name: 'Июль' },
-  { id: '8', name: 'Август' },
-  { id: '9', name: 'Сентябрь' },
-  { id: '10', name: 'Октябрь' },
-  { id: '11', name: 'Ноябрь' },
-  { id: '12', name: 'Декабрь' },
-];
-
 export default function ReportPage() {
   const dispatch = useDispatch();
 
