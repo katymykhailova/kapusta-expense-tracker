e045aeacdfec6e33b86d513f846b6a0d9bde61f8
Maria Shvyrkova <shvyrkovamaria@gmail.com>
2021-11-14 22:04:50 +0200

    fix conflicts

--- src/views/ReportPage/ReportPage.jsx
+++ src/views/ReportPage/ReportPage.jsx
@@ -68,6 +68,7 @@ export default function ReportPage() {
         handleChangeTypeTrans={handleChangeTypeTrans}
         transactionsCurrentMonth={transactions}
       />
+      <BarChartReport />
     </div>
   );
 }
