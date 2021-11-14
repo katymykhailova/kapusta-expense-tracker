a4a55b9c2ff9d2d977bcd298c8917d71c3a7767f
Maria Shvyrkova <shvyrkovamaria@gmail.com>
2021-11-14 18:14:04 +0200

    add charts-component

--- src/views/ReportPage/ReportPage.jsx
+++ src/views/ReportPage/ReportPage.jsx
@@ -2,6 +2,7 @@ import s from './ReportPage.module.css';
 import HeaderReport from '../../components/Reports/HeaderReport';
 import CurrentMonthReport from '../../components/Reports/CurrentMonthReport';
 import HeaderSection from '../../components/HeaderSection/HeaderSection';
+import BarChartReport from 'components/BarChartReport/BarChartReport';
 
 export default function Report() {
   return (
@@ -9,6 +10,7 @@ export default function Report() {
       <HeaderSection typePage="report" />
       <HeaderReport />
       <CurrentMonthReport />
+      <BarChartReport />
     </div>
   );
 }
